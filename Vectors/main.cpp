cd95325a038a1aa6ce0469834971fb7582c6e817
Alilacream <amaramohammedali42@gmail.com>
2026-03-04 17:11:50 +0000

    Tracking commit

--- Vectors/main.cpp
+++ Vectors/main.cpp
@@ -7,5 +7,8 @@ using namespace std;
 int main(int argc, char *argv[]) {
   vector<int> table = {1, 2, 3, 4, 5, 6};
   display(table.back());
+  table.push_back(2);
+  display(table.size());
+
   return EXIT_SUCCESS;
 }
